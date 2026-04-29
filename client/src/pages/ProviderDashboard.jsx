693576d29f5612011df179d27b269c14d95f0222
Ar-waa <ahsanarwa@gmail.com>
2026-04-29 22:07:47 +0600

    allah jane

--- client/src/pages/ProviderDashboard.jsx
+++ client/src/pages/ProviderDashboard.jsx
@@ -36,7 +36,7 @@ const ProviderDashboard = () => {
     // ==========================
     const saveAgreement = async () => {
     try {
-    const res = await fetch("http://https://shohojtrust.onrender.com//api/agreements/draft", {
+    const res = await fetch("https://shohojtrust.onrender.com/api/agreements/draft", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
